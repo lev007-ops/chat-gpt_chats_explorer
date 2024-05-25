2e6e0ea3639bca9849fc475ce9ce62f10f1a6122
Lev <lev_bariakh@icloud.com>
2024-05-25 23:48:12 +0500

    fix

--- start_bot.sh
+++ start_bot.sh
@@ -3,10 +3,10 @@
 if [ -d ".venv" ]; then
     echo "The .venv folder exists."
 else
-    python3.9 -m venv .venv
+    python -m venv .venv
 fi
 
 source .venv/bin/activate
 pip install -r requirements.txt
 pem migrate
-python3.9 bot.py
+python bot.py
